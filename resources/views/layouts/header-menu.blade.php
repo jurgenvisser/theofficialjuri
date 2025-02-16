0863a7f3d33a1ba669e4a815c4c91f1f823df032
Jurgen Visser <jvisser31@noorderpoort.nl>
2025-02-16 18:28:58 +0100

    small tweak on the header-menu. i changed the lines for mobile to all be the same color and opacity by removing the opacity|

--- resources/views/layouts/header-menu.blade.php
+++ resources/views/layouts/header-menu.blade.php
@@ -1,32 +1,32 @@
 <!-- Combined Menu -->
 
     <div class="w-full lg:w-auto block lg:hidden border-b border-colorPrimary lg:border-none"></div>
-    <div class="w-full lg:w-auto border-b border-colorPrimary/85 lg:border-none flex justify-center">
+    <div class="w-full lg:w-auto border-b border-colorPrimary lg:border-none flex justify-center">
         <a href="/" class="lg:animate-underline animate-text-color text-lg font-semibold uppercase py-2 lg:py-0 theme-primary text-center">
             Homepage
         </a>
     </div>
-    <div class="w-full lg:w-auto border-b border-colorPrimary/85 lg:border-none flex justify-center">
+    <div class="w-full lg:w-auto border-b border-colorPrimary lg:border-none flex justify-center">
         <a href="/music" class="lg:animate-underline animate-text-color text-lg font-semibold uppercase py-2 lg:py-0 theme-primary text-center">
             Music
         </a>
     </div>
-    <div class="w-full lg:w-auto border-b border-colorPrimary/70 lg:border-none flex justify-center">
+    <div class="w-full lg:w-auto border-b border-colorPrimary lg:border-none flex justify-center">
         <a href="/streaming" class="lg:animate-underline animate-text-color text-lg font-semibold uppercase py-2 lg:py-0 theme-primary text-center">
             Streaming Services
         </a>
     </div>
-    <div class="w-full lg:w-auto border-b border-colorPrimary/55 lg:border-none flex justify-center">
+    <div class="w-full lg:w-auto border-b border-colorPrimary lg:border-none flex justify-center">
         <a href="/social-media" class="lg:animate-underline animate-text-color text-lg font-semibold uppercase py-2 lg:py-0 theme-primary text-center">
             Social Media
         </a>
     </div>
-    <div class="w-full lg:w-auto border-b border-colorPrimary/85 lg:border-none flex justify-center">
+    <div class="w-full lg:w-auto border-b border-colorPrimary lg:border-none flex justify-center">
         <a href="/company" class="lg:animate-underline animate-text-color text-lg font-semibold uppercase py-2 lg:py-0 theme-primary text-center">
             JURI Bloom
         </a>
     </div>
-    <div class="w-full lg:w-auto border-b border-colorPrimary/25 lg:border-none flex justify-center">
+    <div class="w-full lg:w-auto border-b border-colorPrimary lg:border-none flex justify-center">
         <a href="/contact" class="lg:animate-underline animate-text-color text-lg font-semibold uppercase py-2 lg:py-0 theme-primary text-center">
             Contact
         </a>
