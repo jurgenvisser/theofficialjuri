00620a8c688e154aacdaaf2ce318b175cadd0fce
Jurgen Visser <jvisser31@noorderpoort.nl>
2025-05-29 01:04:36 +0200

    TOJ.0.2.2p This version fixes breakpoints in the footer. Its is not bugged anymore.

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,21 +1,21 @@
 <footer class="bg-black text-white py-8">
     <div class="container mx-auto px-6">
         <!-- Footer content container -->
-        <div class="flex flex-col lg:flex-row justify-between items-center">
+        <div class="flex flex-col xl:flex-row justify-between items-center">
 
             <!-- Logo for larger screens -->
-            <div class="theme-toggle w-80 hidden lg:block">
+            <div class="theme-toggle w-80 hidden xl:block">
                 <p class="font-lexend text-4xl ml-12 text-colorSecondary theme-flipped">@theofficialjuri</p>
             </div>
 
             <!-- Footer content and social media links -->
-            <div class="flex flex-col lg:flex-row justify-between items-center lg:w-2/3 text-center space-y-6 lg:space-y-0">
+            <div class="flex flex-col xl:flex-row justify-between items-center xl:w-2/3 text-center space-y-6 xl:space-y-0">
                 <!-- Social media links -->
-                <div class="text-md lg:text-xl flex flex-col justify-between items-center px-4">
+                <div class="text-md xl:text-xl flex flex-col justify-between items-center px-4">
                     <p class="font-semibold text-center">
                         Follow <span class="text-colorPrimary">JURI</span> & <span class="text-colorPrimary">@theofficialjuri</span> for my music and updates!
                     </p>
-                    <div class="grid grid-cols-2 lg:flex lg:justify-center gap-4 lg:gap-8 font-semibold mt-4 w-full">
+                    <div class="grid grid-cols-2 xl:flex xl:justify-center gap-4 xl:gap-8 font-semibold mt-4 w-full">
                         <!-- Apple Music -->
                         <a href="https://music.apple.com/us/artist/juri/1777135807" target="_blank" class="animate-underline animate-text-color theme-primary">
                             Apple Music
@@ -36,7 +36,7 @@
                             YouTube
                         </a> --}}
                     </div>
-                    <div class="grid grid-cols-2 lg:flex lg:justify-center gap-4 lg:gap-8 font-semibold mt-4 w-full">
+                    <div class="grid grid-cols-2 xl:flex xl:justify-center gap-4 xl:gap-8 font-semibold mt-4 w-full">
                         <!-- Instagram -->
                         <a href="https://www.instagram.com/theofficialjuri" target="_blank" class="animate-underline animate-text-color theme-primary">
                             Instagram
@@ -57,14 +57,14 @@
                 </div>
 
                 <!-- Logo for smaller screens -->
-                <div class="theme-toggle block lg:hidden">
+                <div class="theme-toggle block xl:hidden">
                     <span class="font-lexend text-4xl text-colorSecondary theme-flipped">@theofficialjuri</span>
                 </div>
 
                 <!-- Copyright -->
-                <div class="text-sm mt-6 lg:mt-0">
+                <div class="text-sm mt-6 xl:mt-0">
                     <p class="">&copy; <span id="current-year"></span> <span class="text-colorPrimary">JURI Bloom</span>. All rights reserved.</p>
-                    <p class="text-gray-800">Website Version: TOJ.0.2.1p</p>
+                    <p class="text-gray-800">Website Version: TOJ.0.2.2p</p>
                 </div>
 
             </div>
